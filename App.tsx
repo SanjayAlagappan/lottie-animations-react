5a558b06dcbe2bb84f3ad897a98cdb697f06bf81
SanjayAlagappan <sanjay1283024@gmail.com>
2022-08-01 22:57:26 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -3,36 +3,20 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import { PlayerEvent } from '@lottiefiles/react-lottie-player/dist/src/Player';
 
 export const App = () => {
-  const handleEventPlayer = (e: PlayerEvent) => {
-    if (e === 'load') alert('Lottie-load');
-  };
+  // const handleEventPlayer = (e: PlayerEvent) => {
+  //   if (e === 'load') alert('Lottie-load');
+  // };
 
   const Playerref = React.useRef<Player>(null);
-
+  const [pause,setpause]
   return (
     <div className="container">
       <h1>A maid on her way to serve the dish!</h1>
-         
 
-         <div className="container">
-          
-          <button>
-            </button>
-          
-
-
-
-
-
-           </div>
-
-
-
-
-
-
-
-
+      <div className="container">
+        <button>Play</button>
+        <button>Pause</button>
+      </div>
 
       <Player
         src="https://assets8.lottiefiles.com/packages/lf20_pkghem0h.json"
@@ -43,7 +27,7 @@ export const App = () => {
         background=""
         direction={1}
         style={{ width: '60', height: '60' }}
-        onEvent={handleEventPlayer}
+        // onEvent={handleEventPlayer}
         ref={Playerref}
       />
     </div>
